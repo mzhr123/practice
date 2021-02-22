d2c83579e4799cf92bfe6fb27298534a891c476d
mzhr123 <76685693+mzhr123@users.noreply.github.com>
2021-02-22 16:59:33 +0530

    redux

--- src/Components/FetchingFromRedux/ReduxChild.jsx
+++ src/Components/FetchingFromRedux/ReduxChild.jsx
@@ -6,4 +6,5 @@ export const ReduxChild =()=>{
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro id inventore ea eius ullam corporis perferendis doloribus omnis aut deserunt? Autem soluta quod sapiente expedita voluptatum, ullam quasi cum deleniti.</h1>
         </div>
     );
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum laudantium recusandae culpa. A nobis perferendis facere, sed dolore atque animi natus sapiente deleniti impedit cum ab quo veritatis necessitatibus repellendus.</p>
 };
