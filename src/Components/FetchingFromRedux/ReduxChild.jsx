5df2dd1e72691de593bddf8aaf985abdecbabf6b
mzhr123 <76685693+mzhr123@users.noreply.github.com>
2021-02-25 17:11:00 +0530

    new

--- src/Components/FetchingFromRedux/ReduxChild.jsx
+++ src/Components/FetchingFromRedux/ReduxChild.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ReduxChild =({currentitem})=>{
     return(
-        <div style={{display:"inline-Block",border:"2px solid black",borderRadius:"10px",margin:"10px"}}>
+        <div style={{display:"inline-Block",border:"2px solid black",borderRadius:"10px",margin:"10px",width:"20%"}}>
             <h2>{currentitem.name}</h2>
             <img  src={currentitem.image} style={{width:"30%",height:"30%",border:"2px solid black",borderRadius:"10px"}} />
             <h3>{currentitem.desc}</h3>
