a48af50d94399ec457d40f44764312a37679394a
mzhr123 <76685693+mzhr123@users.noreply.github.com>
2021-03-02 16:33:39 +0530

    msis

--- src/Components/ConditionalRendering/deletingArray/DeleteParent.jsx
+++ src/Components/ConditionalRendering/deletingArray/DeleteParent.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { DeleteChild } from "../deletingArray/DeleteChild";
+import { DeleteChild } from "./DeleteChild";
 
 export const DeleteParent =()=>{
+
+    const defaultPerson = [
+        { name: "mazhar", age: 20 },
+        { name: "saim", age: 27 },
+        { name: "saqib", age: 18 },
+      ];
     return(
         <div>
-        <Deletechild/>
+            <DeleteChild/>
         </div>
     )
 }
--- src/Components/HomePage/Landing.jsx
+++ src/Components/HomePage/Landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { DeleteParent } from "../ConditionalRendering/deletingArray/DeleteParent";
 import { ReduxParent } from "../FetchingFromRedux/ReduxParent";
 
 export const Landing =()=>{
     return(
         <div>
-           <ReduxParent/> 
+           {/* <ReduxParent/>  */}
+           <DeleteParent/>
         </div>
     )
 }
