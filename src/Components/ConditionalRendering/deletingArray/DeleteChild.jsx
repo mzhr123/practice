b086e48546d231d833fea9c0f2b3db6501782813
mzhr123 <76685693+mzhr123@users.noreply.github.com>
2021-03-02 16:27:00 +0530

    new chapter

---
+++ src/Components/ConditionalRendering/deletingArray/DeleteChild.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const DeleteChild =()=>{
+    return(
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, possimus? Beatae, laborum. Error impedit temporibus repellendus dolorum quis esse, inventore vero eum eligendi. Unde saepe asperiores excepturi eligendi dolor fugit.</p>
+        </div>
+
+    )
+}
---
+++ src/Components/ConditionalRendering/deletingArray/DeleteParent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { DeleteChild } from "../deletingArray/DeleteChild";
+
+export const DeleteParent =()=>{
+    return(
+        <div>
+        <Deletechild/>
+        </div>
+    )
+}
